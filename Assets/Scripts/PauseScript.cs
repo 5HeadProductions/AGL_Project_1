69d0305a09283d1b835392a17b466dac43a46b3b
Exopl0x <jacobfactora@yahoo.com>
2021-09-13 02:59:20 -0700

    Added TV 3D model, PauseScreen prefab, PauseScript script.
    To use: Add PauseScreen prefab to any game scene, and put the
    PauseManager script onto any object that isn't PauseScreen.
    Don't forget to set the proper variables in the PauseScript in the
    editor.

---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseScript : MonoBehaviour
+{
+    public GameObject pauseScreen;
+    public static bool paused = false;
+
+    public float pauseTransitionDuration;
+
+    [SerializeField] public Button resumeButton, exitButton;
+    public string exitScene;
+    
+    void Start() {
+        pauseScreen = GameObject.Find("PauseScreen");
+        pauseScreen.SetActive(false);
+
+        resumeButton.onClick.AddListener(ButtonUnpause);        // Button Listeners for resume and exit
+        exitButton.onClick.AddListener(() => SwitchScene(exitScene));
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape) && paused == false) {      // Input handling for the pause to begin/end
+            paused = true;
+            pauseScreen.SetActive(true);
+            StartCoroutine("Pause");
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && paused == true) {
+            paused = false;
+            pauseScreen.SetActive(false);
+            StartCoroutine("Unpause");
+        }
+    }
+
+    IEnumerator Pause() {
+        float timePassed = 0f;
+        
+        while (timePassed < pauseTransitionDuration) {
+            Time.timeScale = Mathf.SmoothStep(1f, 0f, timePassed/pauseTransitionDuration);     // Sick slow motion transition from unpaused to paused.
+            timePassed += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+
+        Time.timeScale = 0;
+    }
+
+    IEnumerator Unpause() {
+        float timePassed = 0f;
+        
+        while (timePassed < pauseTransitionDuration) {
+            Time.timeScale = Mathf.SmoothStep(0f, 1f, timePassed/pauseTransitionDuration);     // Reversed transition from paused to unpaused.
+            timePassed += Time.unscaledDeltaTime;
+
+            yield return null;
+        }
+
+        Time.timeScale = 1;
+    }
+
+    // private void OnGUI() {       // Testing Variables
+    //     GUILayout.Label($"Paused?: {paused}");      
+    //     GUILayout.Label($"Timescale: {Time.timeScale}");
+    // }
+
+    void ButtonUnpause() {      // Resume button calls this function
+        paused = false;
+        pauseScreen.SetActive(false);
+        StartCoroutine("Unpause");
+    }
+
+    void SwitchScene(string scene) {        // Exit button calls this function, remember to set the proper scene name in the 'Exit Scene' field in the editor.
+        SceneManager.LoadScene(scene);
+    }
+}
